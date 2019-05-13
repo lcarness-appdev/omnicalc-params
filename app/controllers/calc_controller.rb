c70d79b207cbad151b340bede4f1eb263eaf57f0
Luca Carnessale <lcarness@chicagobooth.edu>
2019-05-13 20:42:26 +0000

    Halfway through

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -8,5 +8,28 @@ class CalcController < ApplicationController
     @squared_number = @a_number ** 2 
     render("flex_templates/square.html.erb")
   end
-end
+  
+  
+  def flex_square_root
+    @a_number = params.fetch("zebra").to_f.round(0)
+    @square_root_number = (@a_number ** 0.5)
+    render("flex_templates/square_root.html.erb")
+  end
+  
+  def flex_pmt
+    @basis_points = params.fetch("basis_points").to_f / 10000
+    @monthly_basis_points = @basis_points / 12
+    @number_of_years = params.fetch("number_of_years").to_f
+    @number_of_months = @number_of_years * 12
+    @present_value = params.fetch("present_value").to_f
+    @payment = ((@monthly_basis_points * @present_value) / (1 - (1 + @monthly_basis_points) ** (- @number_of_months)))
+    render("flex_templates/pmt.html.erb")
+  end
 
+  def flex_rand
+    @min = params.fetch("min").to_i
+    @max = params.fetch("max").to_i
+    @random_number = rand(@min...@max)
+    render("flex_templates/rand.html.erb")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
     match("/flexible/square/:zebra",{:controller => "calc", :action => "flex_square", :via => "get"})
+    match("/flexible/square_root/:zebra",{:controller => "calc", :action => "flex_square_root", :via => "get"})
+    match("/flexible/payment/:basis_points/:number_of_years/:present_value",{:controller => "calc", :action => "flex_pmt", :via => "get"})
+    match("/flexible/random/:min/:max",{:controller => "calc", :action => "flex_rand", :via =>"get"})
 end
