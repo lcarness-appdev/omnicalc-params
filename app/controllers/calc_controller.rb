e841ad686211914e5f4ae898172ada8655a0e35a
Luca Carnessale <lcarness@chicagobooth.edu>
2019-05-13 22:15:01 +0000

    Almost there

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -1,5 +1,6 @@
 class CalcController < ApplicationController
   
+  
   def flex_square
     # Rails, behind the scenes, creates a variable called "params" and assigns
     # params = {"zebra" => 42}
@@ -15,16 +16,18 @@ class CalcController < ApplicationController
     @square_root_number = (@a_number ** 0.5)
     render("flex_templates/square_root.html.erb")
   end
+
   
   def flex_pmt
     @basis_points = params.fetch("basis_points").to_f / 10000
     @monthly_basis_points = @basis_points / 12
     @number_of_years = params.fetch("number_of_years").to_f
     @number_of_months = @number_of_years * 12
-    @present_value = params.fetch("present_value").to_f
+    @present_value = params.fetch("present_value"). to_f
     @payment = ((@monthly_basis_points * @present_value) / (1 - (1 + @monthly_basis_points) ** (- @number_of_months)))
     render("flex_templates/pmt.html.erb")
   end
+
 
   def flex_rand
     @min = params.fetch("min").to_i
@@ -32,4 +35,51 @@ class CalcController < ApplicationController
     @random_number = rand(@min...@max)
     render("flex_templates/rand.html.erb")
   end
+
+
+  def square_form
+    render("form_templates/square.html.erb")
+  end
+
+  def process_square_form
+    @num = params.fetch("num_to_square").to_f
+    @square_of_num = @num ** 2
+    render("results_templates/square.html.erb")
+  end
+
+
+  def square_root_form
+    render("form_templates/square_root.html.erb")
+  end
+
+  def process_square_root_form
+    @num = params.fetch("num_to_square_root").to_f
+    @square_root_of_num = @num ** 0.5 
+    render("results_templates/square_root.html.erb")
+  end
+
+
+  def pmt_form
+    render("form_templates/pmt.html.erb")
+  end
+  
+  def process_pmt_form
+    @rate = params.fetch("rate").to_f / 12
+    @years = params.fetch("num_of_years").to_f
+    @months = @years * 12
+    @present_value = params.fetch("present_value").to_f
+    @payment = ((@rate * @present_value) / (1 - (1 + @rate) ** (- @months)))
+    render("results_templates/pmt.html.erb")
+  end
+
+
+  def rand_form
+    render("form_templates/rand.html.erb")
+  end
+
+  def process_rand_form
+    render("results_templates/rand.html.erb")
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
     match("/flexible/square_root/:zebra",{:controller => "calc", :action => "flex_square_root", :via => "get"})
     match("/flexible/payment/:basis_points/:number_of_years/:present_value",{:controller => "calc", :action => "flex_pmt", :via => "get"})
     match("/flexible/random/:min/:max",{:controller => "calc", :action => "flex_rand", :via =>"get"})
+    
+    match("/square/new", {:controller => "calc", :action => "square_form", :via => "get"})
+    match("/square/results", {:controller => "calc", :action => "process_square_form", :via => "get"})
+    
+    match("/square_root/new", {:controller => "calc", :action => "square_root_form", :via => "get"})
+    match("/square_root/results", {:controller => "calc", :action => "process_square_root_form", :via => "get"})
+    
+    match("/payment/new", {:controller => "calc", :action => "pmt_form", :via => "get"})
+    match("/payment/results", {:controller => "calc", :action => "process_pmt_form", :via => "get"})
+    
+    match("/random/new", {:controller => "calc", :action => "rand_form", :via => "get"})
+    match("/random/results", {:controller => "calc", :action => "process_rand_form", :via => "get"})
 end
